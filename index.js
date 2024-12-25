10f1978ec178d38b154d92850aaaed1295da786a
Irfankhan20 <sipathan96@gmail.com>
2024-12-25 15:38:34 +0600

    user post method api redesign

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
+//============================================================================
+// mongodb documents
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w7smd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -23,10 +26,28 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+    // user related apis
+    const userCollection = client.db("mobileshop").collection("users");
+
+    //user post api
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({
+          message: "user already exists",
+          insertedId: null,
+        });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
+//============================================================================
 app.get("/", (req, res) => {
   res.send("Hello from mobile shop Server....");
 });
